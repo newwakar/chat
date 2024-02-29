f197b0c3ded13a70d67786e9b99f3ea665a07db1
newwakar <127464384+newwakar@users.noreply.github.com>
2024-02-29 22:27:20 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["http://vizag.tk", "http://www.vizag.tk"],
+		origin: ["http://vizag.tk", "http://www.vizag.tk", "http://www.weebly.com", "http://weebly.com", "https://www.weebly.com", "https://weebly.com"],
 		methods: ["GET", "POST"],
 	},
 });
