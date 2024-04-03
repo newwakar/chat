2f01fe2449fc94e03fcdc6e3b84ad123a36f8f6a
newwakar <127464384+newwakar@users.noreply.github.com>
2024-04-03 15:46:20 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,8 +7,8 @@ const generateTokenAndSetCookie = (userId, res) => {
 
 	res.cookie("jwt", token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
-		httpOnly: True, // prevent XSS attacks cross-site scripting attacks
-		sameSite: Lax,
+		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+    		sameSite: 'Lax',
 		secure:process.env.NODE_ENV !== "development"
 	});
 };
